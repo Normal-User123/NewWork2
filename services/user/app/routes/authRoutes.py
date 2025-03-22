82b0250c57797146f2efd5d7bd41037424c68aee
Normal-User123 <exchange.jiocoin@gmail.com>
2025-03-22 21:45:58 +0530

    Update authRoutes.py

--- services/user/app/routes/authRoutes.py
+++ services/user/app/routes/authRoutes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.schemas import  UserUpdate, User,DeleteUser,updateWalletId,TokenData,UserFilter
 from app.database import get_db
-from app.crud.user import updateUser,getUserById,truncateUsersTable,deleteUser,getAllUsers,deleteAllTable,truncateUserTable,updateUserWalletid,get_level_counts,generatetreeChild,getUserEntitlements
+from app.crud.user import updateUser,getUserById,truncateUsersTable,deleteUser,getAllUsers,deleteAllTable,truncateUserTable,updateUserWalletid,get_level_counts,generatetreeChild,getUserEntitlements,updateUserCoinWalletid
 from typing import Annotated,List
 from app.routes.utils import oauth2_scheme,getTokenDataFromAuthService,modelToSchema
 from starlette.responses import JSONResponse
@@ -108,6 +108,27 @@ async def update_user_wallet(db: Annotated[Session, Depends(get_db)],wallet: upd
     if token_data.flag != "LOGIN":
         return JSONResponse(status_code=401,content={"message":"Invalid Credentials!"})
     updated_user = updateUserWalletid(user_id=token_data.id,wallet_id=wallet.wallet_id,db=db)  # Use the ID from the current user
+    if not updated_user:
+            return JSONResponse(status_code=401,content={'message': "Couldn't update wallet id"})
+    # except Exception as e:
+    #     error_details = traceback.format_exc()  # Captures the full stack trace
+    #     return JSONResponse(status_code=400, content={"error": str(e), "details": error_details})
+    return JSONResponse(status_code=200,content={"message":"wallet_updated successfully!"})
+
+
+@tokenRouter.put("/update/coinwalletid")
+async def update_user_wallet(db: Annotated[Session, Depends(get_db)],wallet: updateWalletId):
+
+
+    # token_data = await getTokenDataFromAuthService(token)
+
+    token_data = wallet.token_data
+    
+    if token_data is None :
+        return JSONResponse(status_code=401,content={"message": "missing Authorization header"})
+    if token_data.flag != "LOGIN":
+        return JSONResponse(status_code=401,content={"message":"Invalid Credentials!"})
+    updated_user = updateUserCoinWalletid(user_id=token_data.id,wallet_id=wallet.wallet_id,db=db)  # Use the ID from the current user
     if not updated_user:
             return JSONResponse(status_code=401,content={'message': "Couldn't update wallet id"})
     # except Exception as e:
